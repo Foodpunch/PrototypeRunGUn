9453b5db0c77d31ac51e300b84f69a76ab75876e
SmallGuillotine <foodpunch@gmail.com>
2021-02-24 14:21:36 +0800

    Initial commit

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    float moveX,moveY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        moveY = (Input.GetAxis("Vertical") > 0.1f) ? 1f : 0f;
+
+
+    }
+}
