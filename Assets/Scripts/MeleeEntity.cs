04178b4d011a219657a4d92dfb15f76f5c80f418
ttokong <ttokong2486@gmail.com>
2021-03-23 16:47:47 +0800

    Added Melee enemies script and group spacing behaviour
    Melee enemies should maintain a specific amount of space between each other while chasing the player

---
+++ Assets/Scripts/MeleeEntity.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeEntity : BaseEntity
+{
+    float stopRadiusSqrd = 16f;
+    float separateSpeed = 0.5f;
+    float separateRadius = 1f;
+    Vector2 sum = Vector2.zero;
+    float count = 0f;
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void DoBehaviour()
+    {
+        float f = Map(distSqrd, 0, speed, 0, stopRadiusSqrd);
+        _rb.velocity = new Vector2(DirectionToPlayer.x, DirectionToPlayer.y) * f;
+
+        var hits = Physics2D.OverlapCircleAll(transform.position, separateRadius);
+        foreach (var hit in hits)
+        {
+            // make sure it is a fellow enemy ** use your enemy script name **
+            if (hit.GetComponent<MeleeEntity>() != null && hit.transform != transform)
+            {
+                // get the difference so you know which way to go
+                Vector2 difference = transform.position - hit.transform.position;
+
+                // weight by distance so being closer means moving more
+                difference = difference.normalized / Mathf.Abs(difference.magnitude);
+
+                // add together to get average of the group
+                // this allows those at the edges of a group to move out while
+                // the enemies in the center of a group to not move much
+                sum += difference;
+                count++;
+            }
+        }
+
+        if (count > 0)
+        {
+            // average the direction
+            sum /= count;
+
+            // set the speed of movement
+            sum = sum.normalized * separateSpeed;
+
+            // this is where you would apply this vector for movement
+            // i am basing this off of the code you provided
+            transform.position = Vector2.MoveTowards(transform.position, transform.position + (Vector3)sum, separateSpeed * Time.deltaTime);
+        }
+    }
+}
