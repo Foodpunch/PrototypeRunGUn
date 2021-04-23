7270cb7425dd65aa7747aa256c1fa541a024f2f0
ttokong <ttokong2486@gmail.com>
2021-04-23 18:25:19 +0800

    Added dialogue system
    Missing click on screen function (to skip text writing ect)

---
+++ Assets/Scripts/CharacterDialogueTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterDialogueTest : MonoBehaviour
+{
+    private Text messageText;
+    public GameObject uiTextMessage;
+    private TextWriter.TextWriterSingle textWriterSingle;
+
+    private void Awake()
+    {
+        messageText = uiTextMessage.GetComponent<Text>();
+    }
+
+    private void Start()
+    {
+        //messageText.text = "Hello World!";
+        textWriterSingle = TextWriter.AddWriterStatic(messageText, "Elijah is a feg", 0.1f, true, true);
+    }
+}
